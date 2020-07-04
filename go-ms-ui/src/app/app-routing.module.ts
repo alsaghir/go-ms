ff4611b5535028d31c5f50bb2b2087329eabb370
Ahmed Alsaghir <ahmed.alsaghir@AssetDev.com>
2020-07-04 20:56:50 +0200

    Some modifications, to be completed !

--- go-ms-ui/src/app/app-routing.module.ts
+++ go-ms-ui/src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {Routes, RouterModule, ExtraOptions} from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'pages',
+    loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)
+  },
+  {
+    path: '',
+    redirectTo: 'pages',
+    pathMatch: 'full'
+  }
+];
+
+const config: ExtraOptions = {
+  useHash: false,
+  enableTracing: true
+};
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, config)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
