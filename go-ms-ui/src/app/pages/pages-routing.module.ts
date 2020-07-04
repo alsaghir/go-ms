297b2e2976f66229f4501e28e2308cdd44e2cdb6
Ahmed Alsaghir <ahmed.alsaghir@AssetDev.com>
2020-07-04 20:58:06 +0200

    Some modifications, to be completed !

---
+++ go-ms-ui/src/app/pages/pages-routing.module.ts
@@ -0,0 +1,29 @@
+import {RouterModule, Routes} from '@angular/router';
+import {NgModule} from '@angular/core';
+import {PagesComponent} from './pages.component';
+import {LoginComponent} from './login/login.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: '',
+        redirectTo: 'admin',
+        pathMatch: 'full'
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class PagesRoutingModule {
+}
